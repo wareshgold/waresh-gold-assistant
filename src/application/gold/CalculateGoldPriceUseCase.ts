3a548f469e222a261a06bbe6e9fdceb966c0032a
Ali Mirzaei <wareshgold@gmail.com>
2026-07-18 10:51:25 +0330

    refactor(application): inject gold rule engine dependency

--- src/application/gold/CalculateGoldPriceUseCase.ts
+++ src/application/gold/CalculateGoldPriceUseCase.ts
@@ -1,32 +1,38 @@
-import { GoldItem } from "../../domain/gold/entities/GoldItem";
-import { Money } from "../../domain/gold/value-objects/Money";
-import { GoldPrice } from "../../domain/gold/value-objects/GoldPrice";
-import { GoldWeight } from "../../domain/gold/value-objects/GoldWeight";
-import { Percentage } from "../../domain/gold/value-objects/Percentage";
-import { createGoldRuleEngine } from "../../domain/gold/services/createGoldRuleEngine";
+import { GoldRuleEngine } from "../../domain/gold/services/GoldRuleEngine";
 
 
 export interface CalculateGoldPriceInput {
+
   weight: number;
+
   goldPrice: number;
+
   laborPercent: number;
+
   profitPercent: number;
+
   taxPercent: number;
+
+  discount?: number;
+
 }
 
 
 export interface CalculateGoldPriceOutput {
+
   total: number;
+
 }
+
 
 
 export class CalculateGoldPriceUseCase {
 
-  private readonly engine;
 
-  constructor() {
-    this.engine = createGoldRuleEngine();
-  }
+  constructor(
+    private readonly engine: GoldRuleEngine
+  ) {}
+
 
 
   execute(
@@ -34,34 +40,19 @@ export class CalculateGoldPriceUseCase {
   ): CalculateGoldPriceOutput {
 
 
-    const goldItem = new GoldItem({
-
-      weight: new GoldWeight(input.weight),
-
-      price:
-        new GoldPrice(
-          new Money(input.goldPrice)
-        ),
-
-      labor:
-        new Percentage(input.laborPercent),
-
-      profit:
-        new Percentage(input.profitPercent),
-
-      tax:
-        new Percentage(input.taxPercent)
-
-    });
-
 
     const result =
-      this.engine.calculate(goldItem);
+      this.engine.execute(input);
+
 
 
     return {
-      total: result.total.amount
+
+      total:
+        result.finalPrice
+
     };
+
   }
 
 }
