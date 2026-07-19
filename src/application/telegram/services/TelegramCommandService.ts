c85b2060c7d93e04dfd2c4cff3287ae41808299d
Ali Mirzaei <wareshgold@gmail.com>
2026-07-19 12:12:49 +0330

    refactor: simplify telegram command service flow

--- src/application/telegram/services/TelegramCommandService.ts
+++ src/application/telegram/services/TelegramCommandService.ts
@@ -1,7 +1,6 @@
 import { TelegramCommandExecutor } from "../interfaces/TelegramCommandExecutor";
 import { TelegramCommandRouter } from "../commands/TelegramCommandRouter";
 import { TelegramCommandContextBuilder } from "../commands/TelegramCommandContextBuilder";
-import { GetGoldPriceUseCase } from "../../usecases/GetGoldPriceUseCase";
 
 
 export class TelegramCommandService
@@ -16,8 +15,7 @@ implements TelegramCommandExecutor {
     constructor(
 
         private readonly router:
-            TelegramCommandRouter |
-            GetGoldPriceUseCase,
+            TelegramCommandRouter,
 
 
         contextBuilder?:
@@ -40,51 +38,16 @@ implements TelegramCommandExecutor {
     ): Promise<any> {
 
 
-
-        if(
-            this.router instanceof TelegramCommandRouter
-        ){
-
-            const context =
-                this.contextBuilder.build(
-                    command
-                );
-
-
-            return this.router.execute(
-                context
+        const context =
+            this.contextBuilder.build(
+                command
             );
 
-        }
 
+        return this.router.execute(
+            context
+        );
 
-
-        if(
-            this.router instanceof GetGoldPriceUseCase
-        ){
-
-            const result =
-                await this.router.execute();
-
-
-
-            return {
-
-                content:
-                `🟡 وارش گلد\n\nقیمت طلا: ${result.gold18Price}`
-
-            };
-
-        }
-
-
-
-        return {
-
-            content:
-            "دستور نامعتبر است"
-
-        };
 
     }
 
