caf3dfb2d964e4af74465cdb7991f6d25d44c126
Ali Mirzaei <wareshgold@gmail.com>
2026-07-28 11:14:47 +0330

    wire telegram command menu service

--- src/bootstrap/factories/createTelegramModule.ts
+++ src/bootstrap/factories/createTelegramModule.ts
@@ -1,130 +1,97 @@
 import { TelegramUpdateMapper }
 from "../../infrastructure/telegram/TelegramUpdateMapper";
 
-
 import { TelegramHttpBotClient }
 from "../../infrastructure/telegram/clients/TelegramHttpBotClient";
-
-
-import { DefaultTelegramNavigationStateService }
-from "../../application/telegram/navigation/TelegramNavigationStateService";
-
 
 import { FakeTelegramBotClient }
 from "../../infrastructure/telegram/FakeTelegramBotClient";
 
-
 import { TelegramCommandRegistry }
 from "../../application/telegram/commands/TelegramCommandRegistry";
-
 
 import { TelegramCommandService }
 from "../../application/telegram/services/TelegramCommandService";
 
+import { TelegramCommandMenuService }
+from "../../application/telegram/services/TelegramCommandMenuService";
 
 import { TelegramMessageHandler }
 from "../../application/telegram/TelegramMessageHandler";
 
-
 import { TelegramResponseFormatter }
 from "../../application/telegram/TelegramResponseFormatter";
-
 
 import { TelegramUpdateProcessor }
 from "../../application/telegram/services/TelegramUpdateProcessor";
 
-
 import { TelegramConversationManager }
 from "../../application/telegram/flows/TelegramConversationManager";
-
 
 import { GoldCalculationConversationFlow }
 from "../../application/telegram/flows/GoldCalculationConversationFlow";
 
-
 import { TelegramWebhookController }
 from "../../interfaces/telegram/TelegramWebhookController";
-
 
 import { TelegramWebhookSecurityGuard }
 from "../../interfaces/telegram/TelegramWebhookSecurityGuard";
 
-
-import { AppEnv }
-from "../../shared/config/env";
-
-
 import { TelegramKeyboardMapper }
 from "../../infrastructure/telegram/TelegramKeyboardMapper";
-
 
 import { TelegramCallbackRegistry }
 from "../../application/telegram/callbacks/TelegramCallbackRegistry";
 
-
 import { TelegramCallbackProcessor }
 from "../../application/telegram/services/TelegramCallbackProcessor";
-
 
 import { TelegramCallbackMapper }
 from "../../application/telegram/mappers/TelegramCallbackMapper";
 
-
 import { TelegramMessageBuilder }
 from "../../application/telegram/presentation/TelegramMessageBuilder";
-
 
 import { MarketAnalyticsMessageFormatter }
 from "../../application/telegram/presentation/MarketAnalyticsMessageFormatter";
 
-
 import { MarketBubbleMessageFormatter }
 from "../../application/telegram/presentation/MarketBubbleMessageFormatter";
 
-
-import { TelegramNavigationService }
+import {
+    TelegramNavigationService,
+    DefaultTelegramNavigationService
+}
 from "../../application/telegram/navigation/TelegramNavigationService";
 
+import { DefaultTelegramNavigationStateService }
+from "../../application/telegram/navigation/TelegramNavigationStateService";
 
-import { DefaultTelegramNavigationService }
-from "../../application/telegram/navigation/TelegramNavigationService";
-
-
-
+import { AppEnv }
+from "../../shared/config/env";
 
 
 
 
 interface Dependencies {
 
+    getGoldPriceUseCase:any;
 
-    getGoldPriceUseCase: any;
+    getGoldBubbleUseCase:any;
 
+    getMarketAnalyticsUseCase:any;
 
-    getGoldBubbleUseCase: any;
+    getMarketHistoryUseCase:any;
 
+    getMarketChartUseCase:any;
 
-    getMarketAnalyticsUseCase: any;
+    calculateGoldFormulaUseCase:any;
 
+    sessionStore:any;
 
-    getMarketHistoryUseCase: any;
-
-
-    getMarketChartUseCase: any;
-
-
-    calculateGoldFormulaUseCase: any;
-
-
-    sessionStore: any;
-
-
-    profileStore: any;
+    profileStore:any;
 
 }
-
-
-
 
 
 
@@ -138,74 +105,47 @@ export function createTelegramModule(
 ) {
 
 
-
-
-
     const conversationManager =
-
 
         new TelegramConversationManager(
 
-
             dependencies.sessionStore,
-
 
             [
 
-
                 new GoldCalculationConversationFlow(
-
 
                     dependencies.sessionStore,
 
-
                     dependencies.calculateGoldFormulaUseCase
-
 
                 )
 
-
             ]
 
-
         );
-
-
-
 
 
 
 
     const marketAnalyticsMessageFormatter =
 
-
         new MarketAnalyticsMessageFormatter(
-
 
             new TelegramMessageBuilder()
 
-
         );
-
-
-
 
 
 
 
     const marketBubbleMessageFormatter =
 
-
         new MarketBubbleMessageFormatter(
-
 
             new TelegramMessageBuilder()
 
-
         );
-
-
-
 
 
 
@@ -214,266 +154,194 @@ export function createTelegramModule(
 
         TelegramNavigationService =
 
-
             new DefaultTelegramNavigationService();
-
-
-
 
 
 
 
     const telegramNavigationStateService =
 
-
         new DefaultTelegramNavigationStateService(
-
 
             dependencies.sessionStore
 
-
         );
-
-
-
 
 
 
 
     const commandRouter =
 
-
         TelegramCommandRegistry.create(
-
 
             dependencies.getGoldPriceUseCase,
 
-
             dependencies.getGoldBubbleUseCase,
-
 
             dependencies.getMarketAnalyticsUseCase,
 
-
             dependencies.getMarketHistoryUseCase,
-
 
             dependencies.calculateGoldFormulaUseCase,
 
-
             dependencies.sessionStore,
-
 
             dependencies.profileStore,
 
-
             marketBubbleMessageFormatter
 
-
         );
-
-
-
 
 
 
 
     const commandService =
 
-
         new TelegramCommandService(
-
 
             commandRouter,
 
-
             conversationManager
 
-
         );
-
-
-
 
 
 
 
     const messageHandler =
 
-
         new TelegramMessageHandler(
-
 
             commandService,
 
-
             new TelegramResponseFormatter()
 
-
         );
-
-
-
 
 
 
 
     const botClient =
 
-
         env.TELEGRAM_BOT_TOKEN
 
-
             ? new TelegramHttpBotClient(
-
 
                 env.TELEGRAM_BOT_TOKEN
 
             )
-
 
             : new FakeTelegramBotClient();
 
 
 
 
+    const commandMenuService =
 
+        new TelegramCommandMenuService(
 
-
-    const callbackRouter =
-
-
-        TelegramCallbackRegistry.create(
-
-
-            dependencies.getGoldPriceUseCase,
-
-
-            dependencies.getGoldBubbleUseCase,
-
-
-            dependencies.getMarketAnalyticsUseCase,
-
-
-            dependencies.getMarketHistoryUseCase,
-
-
-            dependencies.getMarketChartUseCase,
-
-
-            marketAnalyticsMessageFormatter,
-
-
-            marketBubbleMessageFormatter,
-
-
-            telegramNavigationService,
-
-
-            telegramNavigationStateService
-
+            botClient
 
         );
 
 
 
+
+    const callbackRouter =
+
+        TelegramCallbackRegistry.create(
+
+            dependencies.getGoldPriceUseCase,
+
+            dependencies.getGoldBubbleUseCase,
+
+            dependencies.getMarketAnalyticsUseCase,
+
+            dependencies.getMarketHistoryUseCase,
+
+            dependencies.getMarketChartUseCase,
+
+            marketAnalyticsMessageFormatter,
+
+            marketBubbleMessageFormatter,
+
+            telegramNavigationService,
+
+            telegramNavigationStateService
+
+        );
 
 
 
 
     const callbackProcessor =
 
-
         new TelegramCallbackProcessor(
-
 
             new TelegramCallbackMapper(),
 
-
             callbackRouter
 
-
         );
-
-
-
 
 
 
 
     const processor =
 
-
         new TelegramUpdateProcessor(
-
 
             new TelegramUpdateMapper(),
 
-
             messageHandler,
-
 
             new TelegramResponseFormatter(),
 
-
             botClient,
-
 
             new TelegramKeyboardMapper(),
 
-
             callbackProcessor
 
-
         );
-
-
-
 
 
 
 
     const webhookController =
 
-
         new TelegramWebhookController(
-
 
             processor,
 
-
             new TelegramWebhookSecurityGuard(
-
 
                 env.TELEGRAM_WEBHOOK_SECRET
 
             )
-
 
         );
 
 
 
 
-
-
-
     return {
-
 
         messageHandler,
 
-
         telegramMessageHandler:
-
 
             messageHandler,
 
-
         telegramWebhookController:
 
+            webhookController,
 
-            webhookController
+        telegramBotClient:
+
+            botClient,
+
+        commandMenuService
 
     };
 
