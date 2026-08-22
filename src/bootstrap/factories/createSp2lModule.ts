16ac1099a6e3b91284088bcb9ec746dafc22687a
Waresh Gold <wareshgold@gmail.com>
2026-08-22 09:41:09 +0330

    feat: use D1 for SP2L signal persistence

--- src/bootstrap/factories/createSp2lModule.ts
+++ src/bootstrap/factories/createSp2lModule.ts
@@ -2,212 +2,114 @@ import {
     AppEnv
 } from "../../shared/config/env";
 
-
 import {
     SP2LStrategyService
 } from "../../application/strategy/sp2l/SP2LStrategyService";
-
 
 import {
     CollectOunceTickUseCase
 } from "../../application/sp2l/CollectOunceTickUseCase";
 
-
 import {
     CollectOunceTickJob
 } from "../../application/jobs/CollectOunceTickJob";
-
 
 import {
     OunceCandleAggregator
 } from "../../domain/sp2l/services/OunceCandleAggregator";
 
-
 import {
     SP2LSignalEngine
 } from "../../domain/sp2l/services/SP2LSignalEngine";
-
 
 import {
     TelegramOunceMessageProvider
 } from "../../infrastructure/sp2l/TelegramOunceMessageProvider";
 
-
 import {
     TelegramSp2lMarketDataProvider
 } from "../../infrastructure/sp2l/TelegramSp2lMarketDataProvider";
-
 
 import {
     MemoryOunceTickRepository
 } from "../../infrastructure/sp2l/MemoryOunceTickRepository";
 
-
 import {
     D1OunceTickRepository
 } from "../../infrastructure/sp2l/D1OunceTickRepository";
-
 
 import {
     MemorySP2LSignalRepository
 } from "../../infrastructure/sp2l/MemorySP2LSignalRepository";
 
+import {
+    D1SP2LSignalRepository
+} from "../../infrastructure/sp2l/D1SP2LSignalRepository";
 
 import {
     OunceTickRepository
 } from "../../domain/sp2l/repositories/OunceTickRepository";
 
-
-
-
+import {
+    SP2LSignalRepository
+} from "../../domain/sp2l/repositories/SP2LSignalRepository";
 
 export function createSp2lModule(
-
     env: AppEnv
-
 ) {
-
-
-
     const tickRepository:
-
         OunceTickRepository =
-
         env.waresh_gold_db
-
-            ?
-
-            new D1OunceTickRepository(
-
+            ? new D1OunceTickRepository(
                 env.waresh_gold_db
-
             )
-
-            :
-
-            new MemoryOunceTickRepository();
-
-
-
-
-
-
+            : new MemoryOunceTickRepository();
 
     const ounceMessageProvider =
-
         new TelegramOunceMessageProvider(
-
             "https://t.me/s/OunceMarkets"
-
         );
-
-
-
-
-
-
 
     const marketDataProvider =
-
         new TelegramSp2lMarketDataProvider(
-
             ounceMessageProvider,
-
             tickRepository,
-
-            new OunceCandleAggregator(
-
-                5
-
-            )
-
+            new OunceCandleAggregator(5)
         );
 
-
-
-
-
-
-
-    const signalRepository =
-
-        new MemorySP2LSignalRepository();
-
-
-
-
-
-
+    const signalRepository:
+        SP2LSignalRepository =
+        env.waresh_gold_db
+            ? new D1SP2LSignalRepository(
+                env.waresh_gold_db
+            )
+            : new MemorySP2LSignalRepository();
 
     const strategyService =
-
         new SP2LStrategyService(
-
             new SP2LSignalEngine(),
-
             marketDataProvider,
-
             signalRepository
-
         );
-
-
-
-
-
-
 
     const collectOunceTickUseCase =
-
         new CollectOunceTickUseCase(
-
             ounceMessageProvider,
-
             tickRepository
-
         );
-
-
-
-
-
-
 
     const collectOunceTickJob =
-
         new CollectOunceTickJob(
-
             collectOunceTickUseCase
-
         );
 
-
-
-
-
-
-
     return {
-
-
         tickRepository,
-
-
         ounceMessageProvider,
-
-
         marketDataProvider,
-
-
+        signalRepository,
         strategyService,
-
-
         collectOunceTickUseCase,
-
-
         collectOunceTickJob
-
-
     };
-
 }
