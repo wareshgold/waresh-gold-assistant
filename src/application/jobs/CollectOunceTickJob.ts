ae43c7aa77649ddff1fc84a0ea95f969fa7d6c2b
Your Name <you@example.com>
2026-08-23 14:58:20 +0330

    refactor(job): decouple ounce tick job from strategy

--- src/application/jobs/CollectOunceTickJob.ts
+++ src/application/jobs/CollectOunceTickJob.ts
@@ -1,11 +1,11 @@
-import {
-    CollectOunceTickUseCase
-} from "../sp2l/CollectOunceTickUseCase";
+export interface CollectOunceTickExecutable {
+    execute(): Promise<unknown>;
+}
 
 export class CollectOunceTickJob {
 
     constructor(
-        private readonly collectOunceTickUseCase: CollectOunceTickUseCase
+        private readonly collectOunceTickUseCase: CollectOunceTickExecutable
     ) {}
 
     async execute() {
