b98495070fe3a195d73e35c75666d3a97657b2a2
Waresh Gold <wareshgold@gmail.com>
2026-08-23 16:00:58 +0330

    feat(telegram): add strategy submenu

---
+++ src/application/telegram/menu/TelegramStrategyMenu.ts
@@ -0,0 +1,16 @@
+import { TelegramMenuItem } from "./TelegramMenuItem";
+import { TelegramMenuActionType } from "./TelegramMenuAction";
+import { TelegramBackMenuItem } from "./TelegramMenuItems";
+import { NavigationAction } from "../navigation/NavigationAction";
+
+export const TelegramStrategyMenu: TelegramMenuItem[] = [
+    {
+        id: "strategy.sp2l",
+        label: "📈 استراتژی SP2L",
+        action: {
+            type: TelegramMenuActionType.CALLBACK,
+            value: NavigationAction.SP2L,
+        },
+    },
+    TelegramBackMenuItem,
+];
