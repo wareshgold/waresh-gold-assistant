1bf18a6669ac7b2e7c61f76a9bff2f3e288eb62e
Waresh Gold <wareshgold@gmail.com>
2026-08-23 08:47:48 +0330

    refactor(telegram): standardize compact numeric presentation

--- src/application/telegram/presentation/TelegramNumberFormatter.ts
+++ src/application/telegram/presentation/TelegramNumberFormatter.ts
@@ -2,105 +2,42 @@ export class TelegramNumberFormatter {
 
 
     format(
-
-        value:
-            number
-
+        value: number
     ): string {
-
-
         return new Intl.NumberFormat(
-
-            "fa-IR"
-
-        )
-
-        .format(
-
+            "en-US"
+        ).format(
             Math.round(value)
-
         );
-
-
     }
-
-
-
 
 
     formatCode(
-
-        value:
-            number
-
+        value: number
     ): string {
-
-
         return `<code>${this.format(value)}</code>`;
-
-
     }
-
-
-
 
 
     copyValue(
-
-        value:
-            number
-
+        value: number
     ): string {
-
-
-        return new Intl.NumberFormat(
-
-            "en-US"
-
-        )
-
-        .format(
-
+        return String(
             Math.round(value)
-
         );
-
-
     }
-
-
-
 
 
     money(
-
-        value:
-            number
-
+        value: number
     ): string {
-
-
         return `${this.formatCode(value)} تومان`;
-
-
     }
-
-
-
 
 
     weight(
-
-        value:
-            number
-
+        value: number
     ): string {
-
-
         return `${value.toFixed(3)} گرم`;
-
-
     }
-
-
 }
