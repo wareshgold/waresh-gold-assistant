f9242240be5227e690ff3e366e02d231d1c5e2aa
Waresh Gold <wareshgold@gmail.com>
2026-09-03 14:56:24 +0330

    fix(gold): propagate optional calculator percentages

--- src/domain/gold/services/GoldRuleEngineService.ts
+++ src/domain/gold/services/GoldRuleEngineService.ts
@@ -2,66 +2,26 @@ import { GoldRuleEngine } from "./GoldRuleEngine";
 import { GoldFormulaCalculator } from "./GoldFormulaCalculator";
 import { TaxMode } from "../value-objects/Tax";
 
+export class GoldRuleEngineService implements GoldRuleEngine {
+  private readonly calculator = new GoldFormulaCalculator();
 
-export class GoldRuleEngineService
-implements GoldRuleEngine {
-
-
-    private readonly calculator =
-        new GoldFormulaCalculator();
-
-
-
-    execute(input: {
-
-        weight: number;
-
-        goldPrice: number;
-
-        laborPercent: number;
-
-        profitPercent: number;
-
-        taxPercent: number;
-
-        taxMode?: TaxMode;
-
-        discount?: number;
-
-    }) {
-
-
-        return this.calculator.calculate({
-
-            weight:
-                input.weight,
-
-
-            goldPrice:
-                input.goldPrice,
-
-
-            laborPercent:
-                input.laborPercent,
-
-
-            profitPercent:
-                input.profitPercent,
-
-
-            taxPercent:
-                input.taxPercent,
-
-
-            taxMode:
-                input.taxMode,
-
-
-            discount:
-                input.discount
-
-        });
-
-    }
-
+  execute(input: {
+    weight: number;
+    goldPrice: number;
+    laborPercent?: number;
+    profitPercent?: number;
+    taxPercent?: number;
+    taxMode?: TaxMode;
+    discount?: number;
+  }) {
+    return this.calculator.calculate({
+      weight: input.weight,
+      goldPrice: input.goldPrice,
+      laborPercent: input.laborPercent,
+      profitPercent: input.profitPercent,
+      taxPercent: input.taxPercent,
+      taxMode: input.taxMode,
+      discount: input.discount,
+    });
+  }
 }
