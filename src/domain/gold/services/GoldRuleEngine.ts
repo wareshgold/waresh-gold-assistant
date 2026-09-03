95ed8dfd38b7354b010bee321ba841dd77e3d8f5
Waresh Gold <wareshgold@gmail.com>
2026-09-03 14:55:17 +0330

    fix(gold): make rule engine percentages optional

--- src/domain/gold/services/GoldRuleEngine.ts
+++ src/domain/gold/services/GoldRuleEngine.ts
@@ -1,52 +1,19 @@
 import { TaxMode } from "../value-objects/Tax";
 
-
 export interface GoldRuleEngine {
-
-
   execute(input: {
-
-
     weight: number;
-
-
     goldPrice: number;
-
-
-    laborPercent: number;
-
-
-    profitPercent: number;
-
-
-    taxPercent: number;
-
-
+    laborPercent?: number;
+    profitPercent?: number;
+    taxPercent?: number;
     taxMode?: TaxMode;
-
-
     discount?: number;
-
-
   }): {
-
-
     goldValue: number;
-
-
     labor: number;
-
-
     profit: number;
-
-
     tax: number;
-
-
     finalPrice: number;
-
-
   };
-
-
 }
