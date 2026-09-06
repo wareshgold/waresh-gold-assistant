167a2ba13d23e1e3f05bc97b40cb55f8c913d8b0
Waresh Gold <wareshgold@gmail.com>
2026-09-06 11:56:05 +0330

    feat(account): add customer address domain entity

---
+++ src/domain/customer/entities/CustomerAddress.ts
@@ -0,0 +1,13 @@
+export type CustomerAddress = {
+    id: string;
+    customerId: string;
+    title: string;
+    recipientName: string;
+    phone: string;
+    province: string;
+    city: string;
+    address: string;
+    postalCode: string;
+    createdAt: string;
+    updatedAt: string;
+};
