a46f032edf083b945b0cb841de868c68c798a3ce
Waresh Gold <wareshgold@gmail.com>
2026-09-07 11:34:38 +0330

    feat: add customer order history repository

--- src/domain/catalog/repositories/OrderRepository.ts
+++ src/domain/catalog/repositories/OrderRepository.ts
@@ -4,4 +4,5 @@ export interface OrderRepository {
     save(order: Order): Promise<void>;
     findById(orderId: string): Promise<Order | null>;
     findByQuoteId(quoteId: string): Promise<Order | null>;
+    findByCustomerId(customerId: string): Promise<Order[]>;
 }
